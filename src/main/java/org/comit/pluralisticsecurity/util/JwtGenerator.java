b41d2dcc5554246d11db1e42620c268f4b0d3a79
alexf <letsdevjava@gmail.com>
2023-10-25 15:36:25 -0600

    add_jwt_gen

--- src/main/java/org/comit/pluralisticsecurity/util/JwtGenerator.java
+++ src/main/java/org/comit/pluralisticsecurity/util/JwtGenerator.java
@@ -1,2 +1,24 @@
-package org.comit.pluralisticsecurity.util;public class JwtGenerator {
+package org.comit.pluralisticsecurity.util;
+
+import org.comit.pluralisticsecurity.entity.User;
+import org.comit.pluralisticsecurity.service.JWTService;
+import org.comit.pluralisticsecurity.service.impl.JWTServiceimpl;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+
+public class JwtGenerator {
+    public static void main(String[] args) {
+        // Create an instance of your JWTService implementation
+        JWTService jwtService = new JWTServiceimpl();
+
+        // Create a UserDetails object (you can mock this for testing)
+        UserDetails userDetails = new User("test@gmail.com", "test", new ArrayList<>());
+
+        // Generate a JWT token
+        String jwtToken = jwtService.generateToken(userDetails);
+
+        // Print the JWT token
+        System.out.println("Generated JWT Token: " + jwtToken);
+    }
 }
